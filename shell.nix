77e321404044d6e460cea6eb6107f912bdf46417
NotAShelf <raf@notashelf.dev>
2026-09-19 23:02:53 +0300

    nix: drop Rust toolchain from devshell; add shellcheck
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I95d23100295eaa1d6c305540a3714cce6a6a6964

--- shell.nix
+++ shell.nix
@@ -4,12 +4,6 @@ pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
     just
     jdk21
-
-    # Yes this mf vendors Rust. No you can't see it.
-    # Why? Because I hate it. Don't complain.
-    rustc
-    cargo
-    clippy
-    rustfmt
+    shellcheck
   ];
 }
